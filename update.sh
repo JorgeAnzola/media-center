bb7e27e52ea3f22b5ac46726ff98b3637a97d9de
Jorge Anzola <jorgeanzola@gmail.com>
2021-10-27 09:36:52 +0200

    Changed the update script

--- update.sh
+++ update.sh
@@ -1,42 +1,12 @@
 #!/usr/bin/env bash
 
 echo '=============================='
-echo 'Updating Plex image'
+echo 'Updating container images'
 echo '=============================='
-docker pull ghcr.io/linuxserver/plex
+docker-compose pull
 
 echo '=============================='
-echo 'Updating Jackett image'
-echo '=============================='
-docker pull ghcr.io/linuxserver/jackett
-
-echo '=============================='
-echo 'Updating Radarr image'
-echo '=============================='
-docker pull ghcr.io/linuxserver/radarr
-
-echo '=============================='
-echo 'Updating Sonarr image'
-echo '=============================='
-docker pull ghcr.io/linuxserver/sonarr
-
-echo '=============================='
-echo 'Updating Transmission image'
-echo '=============================='
-docker pull ghcr.io/linuxserver/transmission
-
-echo '=============================='
-echo 'Updating Tautulli image'
-echo '=============================='
-docker pull ghcr.io/linuxserver/tautulli
-
-echo '=============================='
-echo 'Stopping the containers'
-echo '=============================='
-docker-compose down
-
-echo '=============================='
-echo 'Starting the containers'
+echo 'Restarting the containers'
 echo '=============================='
 docker-compose up -d
 
@@ -49,4 +19,3 @@ if [ "$str1" == "$str2" ]; then
     echo '=============================='
     docker image prune --all
 fi
-
